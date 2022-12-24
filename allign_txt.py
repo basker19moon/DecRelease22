10a52d6f041a85d77e0f7de675a98605290a8657
basker19moon <basker19moon@gmail.com>
2022-12-24 18:13:59 +0530

    added spaces again

--- allign_txt.py
+++ allign_txt.py
@@ -1,7 +1,11 @@
 import os
 
 t_w = os.get_terminal_size().columns
+
 given_str=input("Enter your String: ")
+
 print(given_str.center(t_w).title())
+
 print(given_str.ljust(t_w).title())
+
 print(given_str.rjust(t_w).title())
